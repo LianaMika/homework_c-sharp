c3c3ea30c438db54d27829788978abf8a9d15ee9
Liana Mikanba <ileemknb@gmail.com>
2023-06-03 20:08:12 +0300

    Правки

---
+++ homework-1 copy/task1/Program.cs
@@ -0,0 +1,36 @@
+﻿﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+// a = -9 b = -3 -> max = -3
+
+// Console.WriteLine ("введите первое число");
+// string input1 = Console.ReadLine();
+// int number1 = Convert.ToInt32(input1);
+// Console.WriteLine ("введите второе число");
+// string input2 = Console.ReadLine();
+// int number2 = Convert.ToInt32(input2);
+// int Max = number1;
+// if (Max>number2)
+// {
+//     Console.WriteLine ("Максимальное число {0}",number1);
+// }
+// else 
+// Console.WriteLine ("Максимальное число {0}",number2);
+
+
+
+Console.WriteLine ("введите первое число");
+string input1 = Console.ReadLine();
+int number1 = Convert.ToInt32(input1);
+Console.WriteLine ("введите второе число");
+string input2 = Console.ReadLine();
+int number2 = Convert.ToInt32(input2);
+int max = number1;
+int min = number2;
+if (max < number2)
+{
+    max = number2;
+    min = number1;
+}
+Console.WriteLine ("Большее число " + max);
+Console.WriteLine ("Меньшее число " + min);
---
+++ homework-1 copy/task2/Program.cs
@@ -0,0 +1,24 @@
+﻿﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.WriteLine ("введите первое число");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("введите второе число");
+int number2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine ("введите третье число");
+int number3 = Convert.ToInt32(Console.ReadLine());
+
+int max = number1;
+
+if (max < number2)
+{
+    max = number2;
+}
+if (max < number3);
+{
+    max = number3;
+}
+
+Console.WriteLine ("Максимальное число " + max);
