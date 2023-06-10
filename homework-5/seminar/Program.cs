9c967ffa267056ef1a19238c5507b36cd3058bd1
Liana Mikanba <ileemknb@gmail.com>
2023-06-11 00:20:03 +0300

    save

--- homework-5/seminar/Program.cs
+++ homework-5/seminar/Program.cs
@@ -1,2 +1,57 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 31: Задайте массив из 12 элементов, заполненный случайными числами из промежутка [-9, 9]. Найдите сумму отрицательных и положительных элементов массива.
+// Например, в массиве [3,9,-8,1,0,-7,2,-1,8,-3,-1,6] сумма положительных чисел равна 29, сумма отрицательных равна -20.
+
+
+int[] GetArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(-9, 10);
+    }
+    return array;
+}
+
+int GetLength()
+{
+    Console.WriteLine("Введите длину");
+    int length = Convert.ToInt32(Console.ReadLine());
+    return length;
+}
+
+int SumPositive(int[] array)
+{
+    int sum = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            sum += array[i];
+        }
+    }
+    return sum;
+}
+
+int SumNegative(int[] array)
+{
+    int sum = 0;
+
+    foreach (int number in array)
+    {
+        if (number < 0)
+        {
+            sum += number;
+        }
+    }
+
+    return sum;
+}
+
+int length = GetLength();
+int[] array = GetArray(length);
+int sumPositive = SumPositive(array);
+int sumNegative = SumNegative(array);
+string arrayToStr = String.Join(",", array);
+
+
+Console.WriteLine($"Сумма положительных {sumPositive}, сумма отрицательных {sumNegative}, массив: {arrayToStr}");
--- homework-5/task4/Program.cs
+++ homework-5/task4/Program.cs
@@ -4,34 +4,36 @@
 // [1, 2, 3, 6, 2] -> 0
 // [10, 11, 12, 13, 14] -> 5
 
-// int[] GetArray(int length)
-// {
-//     int[] array = new int[length];
-//     for (int i = 0; i < length; i++)
-//         {
-//             array[i] = new Random().Next(0, 1001);
-//         }
-//     return array;
-// }
+int[] GetArray(int length)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+        {
+            array[i] = new Random().Next(0, 1001);
+        }
+    return array;
+}
 
-// int GetLength()
-// {
-//     Console.WriteLine("Введите длину");
-//     int length = Convert.ToInt32(Console.ReadLine());
-//     return length;
-// }
+int GetLength()
+{
+    Console.WriteLine("Введите длину");
+    int length = Convert.ToInt32(Console.ReadLine());
+    return length;
+}
 
-// int count = 0;
-// for (int i = 0; i < length; i++) 
-// {
-//     if (array[i] >= 10 && array[i] <= 99)
-//     {
-//         count++;
-//     }
+int count = 0;
+for (int i = 0; i < length; i++) 
+{
+    if (array[i] >= 10 && array[i] <= 99)
+    {
+        count++;
+    }
 
-//     Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [10, 99] {count}");   
+    return 0;
+}
 
-//     return 0;
-// }
+// int length = GetLength();
+// int[] array = GetArray(length);
+// string arrayToStr = String.Join(",", array);
 
-
+Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [10, 99] {count}");   
