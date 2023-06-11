02dfe63ae818abb4efb4a81828319a39650bc0d7
Liana Mikanba <ileemknb@gmail.com>
2023-06-11 23:35:27 +0300

    save

--- homework-5/task4/Program.cs
+++ homework-5/task4/Program.cs
@@ -5,50 +5,89 @@
 // [1, 2, 3, 6, 2] -> 0
 // [10, 11, 12, 13, 14] -> 5
 
-class Program
+// class Program
+// {
+//     static int[] GetArray(int length)
+//     {
+//         int[] array = new int[length];
+//         Random random = new Random();
+
+//         for (int i = 0; i < length; i++)
+//         {
+//             array[i] = random.Next(0, 1000);
+//         }
+
+//         return array;
+//     }
+
+//     static int GetLength()
+//     {
+//         Console.WriteLine("Введите длину:");
+//         int length = Convert.ToInt32(Console.ReadLine());
+//         return length;
+//     }
+
+//     static int Count(int[] array)
+//     {
+//         int count = 0;
+//         for (int i = 0; i < array.Length; i++)
+//         {
+//             if (array[i] >= 10 && array[i] <= 99)
+//             {
+//                 count++;
+//             }
+//         }
+
+//         return count;
+//     }
+
+//     static void Main()
+//     {
+//         int length = GetLength();
+//         int[] array = GetArray(length);
+//         int count = Count(array);
+//         string arrayToStr = string.Join(", ", array);
+
+//         Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [10, 99]: {count}");
+//         // Console.WriteLine($"Массив: {arrayToStr}");
+//     }
+// }
+
+int[] GetArray(int length)
 {
-    static int[] GetArray(int length)
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
     {
-        int[] array = new int[length];
-        Random random = new Random();
-
-        for (int i = 0; i < length; i++)
-        {
-            array[i] = random.Next(0, 1000);
-        }
-
-        return array;
+        array[i] = new Random().Next(0, 1000);
     }
-
-    static int GetLength()
-    {
-        Console.WriteLine("Введите длину:");
-        int length = Convert.ToInt32(Console.ReadLine());
-        return length;
-    }
-
-    static int Count(int[] array)
-    {
-        int count = 0;
-        for (int i = 0; i < array.Length; i++)
-        {
-            if (array[i] >= 10 && array[i] <= 99)
-            {
-                count++;
-            }
-        }
-
-        return count;
-    }
-
-    static void Main()
-    {
-        int length = GetLength();
-        int[] array = GetArray(length);
-        int count = Count(array);
-        string arrayToStr = string.Join(", ", array);
-
-        Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [10, 99]: {count}");
-        // Console.WriteLine($"Массив: {arrayToStr}");
-    }
+    return array;
 }
+
+int GetLength()
+{
+    Console.WriteLine("Введите длину");
+    int length = Convert.ToInt32(Console.ReadLine());
+    return length;
+}
+
+int Count(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++) 
+    {
+        if (array[i] >= 10 && array[i] <= 99)
+        {
+            count++;
+        }
+    }
+
+    return count;
+    
+}
+
+int length = GetLength();
+int[] array = GetArray(length);
+int count = Count(array);
+string arrayToStr = String.Join(",", array);
+
+Console.WriteLine($"Количество элементов массива, значения которых лежат в отрезке [10, 99]: {count}");
