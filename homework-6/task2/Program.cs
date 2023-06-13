59c60a4175973b354d364302d6b3beca7bb914e2
Liana Mikanba <ileemknb@gmail.com>
2023-06-13 19:16:13 +0300

    save

--- homework-6/task2/Program.cs
+++ homework-6/task2/Program.cs
@@ -16,27 +16,24 @@ if (ValidateLines(lineData1, lineData2))
 {
 	
 	double[] coord = FindCoords(lineData1, lineData2);
-	Console.Write($”Точка пересечения уравнений
-	y={ lineData1[COEFFICIENT]}*x+{lineData1[CONSTANT]} и
-	y={ lineData1[COEFFICIENT]}*x+{lineData2[CONSTANT]} “);
-	Console.WriteLine($” имеет координаты ({coord[X_COORD]}, ({coord[Y_COORD]})”);
-
-Double Prompt(string message)
-{
-	System.Console.Write(message); //вывести сообщение
-	string value = Console.ReadLine(); //считывает с консоли строку 
-	int result = Convert.ToInt32value; //преобразует строку в целое число
-	
-return result; //возвращает результат
+	Console.Write($"Точка пересечения уравнений y = {lineData1[COEFFICIENT]} * x + {lineData1[CONSTANT]} и y = {lineData1[COEFFICIENT]} * x + {lineData2[CONSTANT]} ");
+	Console.WriteLine($" имеет координаты ({coord[X_COORD]}), ({coord[Y_COORD]})");
 }
 
-//Ввод данных по прямой
+double Prompt(string message)
+{
+	System.Console.Write(message);
+	string value = Console.ReadLine();
+	double result = Convert.ToDouble(value); 
+	
+    return result; 
+}
 
 double[] InputLineData(int numberOfLine)
 {
 	double[] lineData = new double[2];
-	lineData[COEFFICIENT] = Prompt($”Введите коэффициент для {numberOfLine} прямой >”);
-	lineData[CONSTANT] = Prompt($”Введите константу для {numberOfLine} прямой >”);
+	lineData[COEFFICIENT] = Prompt($"Введите коэффициент для {numberOfLine} прямой >");
+	lineData[CONSTANT] = Prompt($"Введите константу для {numberOfLine} прямой >");
 	return lineData;
 }
 
@@ -44,7 +41,7 @@ double[] InputLineData(int numberOfLine)
 double[] FindCoords(double[] lineData1, double[] lineData2)
 {
 	double[] coord = new double[2];
-	coord[X_COORD] = (lineData1[CONSTANT] – lineData2[CONSTANT]) / (lineData2[COEFFICIENT] – lineData1[COEFFICIENT]);
+	coord[X_COORD] = (lineData1[CONSTANT] - lineData2[CONSTANT]) / (lineData2[COEFFICIENT] - lineData1[COEFFICIENT]);
 coord[Y_COORD] = lineData1[CONSTANT] * coord[X_COORD] + lineData1[CONSTANT];
 	return coord;
 }
@@ -56,12 +53,12 @@ bool ValidateLines(double[]lineData1, double[]lineData2)
 		if (lineData1[CONSTANT] == lineData2[CONSTANT])
 
 		{
-			Console.WriteLine(“Прямые совпадают”);
+			Console.WriteLine("Прямые совпадают");
 			return false;
 		}
 		else
 		{ 
-Console.WriteLine(“Прямые параллельны”);
+Console.WriteLine("Прямые параллельны");
 			return false;
 		}
 	}
