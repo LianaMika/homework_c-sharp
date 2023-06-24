2968ce3cf8110d44291e750b4bc68f7e4adef46a
Liana Mikanba <ileemknb@gmail.com>
2023-06-24 23:08:13 +0300

    save

--- homework-9/task3/Program.cs
+++ homework-9/task3/Program.cs
@@ -3,26 +3,47 @@
 // m = 3, n = 2 -> A(m,n) = 29
 
 
-Console.Clear();
+// Console.Clear();
+// int Prompt(string message)
+// {
+//     Console.Write(message);
+//     int result = Convert.ToInt32(Console.ReadLine());
+//     return result;
+// }
+
+// int Akkerman(int m, int n)  
+
+// {
+//     if (m == 0) return n + 1;
+//     if (m > 0 && n == 0) return Akkerman(m - 1, 1);
+//     else return Akkerman(m - 1, Akkerman(m, n - 1));
+// }
+
+// int m = Prompt("Input M: ");
+// int n = Prompt("Input N: ");
+
+// int result = Akkerman(m, n);
+
+// Console.WriteLine($"A({m},{n}) = {result}");
+
+
 int Prompt(string message)
 {
     Console.Write(message);
-    int result = Convert.ToInt32(Console.ReadLine());
+    int result;
+    while (!int.TryParse(Console.ReadLine(), out result) || result < 0)
+    {
+        Console.WriteLine("Invalid input. Please enter a valid positive integer.");
+        Console.Write(message);
+    }
     return result;
 }
 
-int Akkerman(int m, int n)  
-
+int Akkerman(int m, int n)
 {
-    // if (m < 0 || n < 0)
-    // {
-    //     Console.WriteLine("Введите положительное число!");
-    //     return -1; // or any other appropriate value to indicate an error
-    // }
-
     if (m == 0) return n + 1;
     if (m > 0 && n == 0) return Akkerman(m - 1, 1);
-    else return Akkerman(m - 1, Akkerman(m, n - 1));
+    return Akkerman(m - 1, Akkerman(m, n - 1));
 }
 
 int m = Prompt("Input M: ");
